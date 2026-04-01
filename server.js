ce963a3df8e09c274fdd09221ada971e2197ddc0
ivankemei <ivankemei3@gmail.com>
2026-04-01 15:42:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -221,9 +221,9 @@ app.post("/register", async (req, res) => {
   if (phone.startsWith("+254")) phone = phone.substring(1);
 
   const prices = {
-    skill_showcase: 5,
-    head_to_head: 5,
-    spectator: 5
+    skill_showcase: 1000,
+    head_to_head: 1000,
+    spectator: 500
   };
 
   const amount = prices[ticketType] || 50;
